55d59379f8ca848ca1d9ed122e6126befaa9b964
clach04 <clach04@gmail.com>
2023-02-13 20:20:57 -0800

    Config file support for ptcat

--- puren_tonbo/tools/ptcat.py
+++ puren_tonbo/tools/ptcat.py
@@ -3,7 +3,14 @@
 # vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
 """Command line tool to decrypt to stdout Puren Tonbo files (Tombo CHI Blowfish files, VimCrypt, AES-256.zip, etc.)
 
-python2 -m puren_tonbo.tools.ptcat -p test test.chi
+    python -m puren_tonbo.tools.ptcat -h
+    python -m puren_tonbo.tools.ptcat -p test test.chi
+
+TODO consider arbitary (absolute, possibly even relative "../../") paths as command line, avoiding directory sandbox sanity check (FileSystemNotes.abspath2relative())?
+i.e. just like ptcipher, e.g.:
+
+    python -m puren_tonbo.tools.ptcat -p test /tmp/test.chi
+    python -m puren_tonbo.tools.ptcat -p test C:\tmp\test.chi
 
 """
 
@@ -27,10 +34,11 @@ def main(argv=None):
     usage = "usage: %prog [options] in_filename"
     parser = OptionParser(usage=usage, version="%%prog %s" % puren_tonbo.__version__)
     parser.add_option("--list-formats", help="Which encryption/file formats are available", action="store_true")
-    parser.add_option("--note-root", help="Direcory of notes", default='.')  # TODO pick up from a config file
-    parser.add_option("-c", "--codec", help="File encoding", default='utf-8')
+    parser.add_option("--note-root", help="Directory of notes override")
+    parser.add_option("-c", "--codec", help="Override config file encoding (can be a list TODO format comma?)")
     parser.add_option("-p", "--password", help="password, if omitted but OS env PT_PASSWORD is set use that, if missing prompt")
     parser.add_option("-P", "--password_file", help="file name where password is to be read from, trailing blanks are ignored")
+    parser.add_option("--config-file", help="Override config file")
     parser.add_option("-v", "--verbose", action="store_true")
 
     (options, args) = parser.parse_args(argv[1:])
@@ -69,10 +77,20 @@ def main(argv=None):
     if password and not callable(password) and not isinstance(password, bytes):
         password = password.encode('us-ascii')
 
-    note_encoding = options.codec
+    config = puren_tonbo.get_config(options.config_file)
 
-    note_root = puren_tonbo.FileSystemNotes(options.note_root, note_encoding)
-    data = note_root.note_contents(in_filename, password)
+    if options.codec:
+        note_encoding = options.codec
+    else:
+        note_encoding = config['codec']
+
+    if options.note_root:
+        note_root = options.note_root
+    else:
+        note_root = config.get('note_root', '.')
+
+    notes = puren_tonbo.FileSystemNotes(note_root, note_encoding)
+    data = notes.note_contents(in_filename, password)
     #print('%r' % data)
     print('%s' % data)
 
