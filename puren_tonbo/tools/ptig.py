883d578f5070bb6060e69ab708804925b1d14eaa
clach04 <clach04@gmail.com>
2023-03-19 09:52:33 -0700

    start of ptig interactive Cmd

--- puren_tonbo/tools/ptig.py
+++ puren_tonbo/tools/ptig.py
@@ -7,11 +7,20 @@ Example encryption file formats; Tombo CHI Blowfish files, VimCrypt, AES-256.zip
     python -m puren_tonbo.tools.ptig -h
 """
 
+import json
 import os
 from optparse import OptionParser
+import shlex
 import sys
 import time
 
+try:
+    from cmd2 import Cmd  # version 0.8.9
+except ImportError:
+    # use stdlib
+    from cmd import Cmd
+
+# FIXME don't do this, rely on ptgrep
 try:
     import colorama
 except ImportError:
@@ -19,17 +28,103 @@ except ImportError:
 
 import puren_tonbo
 from puren_tonbo import SearchCancelled
+from puren_tonbo.tools import ptgrep
 
 is_py3 = sys.version_info >= (3,)
 is_win = sys.platform.startswith('win')
+
+
+class CommandPrompt(Cmd):
+    def __init__(self, paths_to_search=None, pt_config=None, grep_options=None):  # TODO refactorm too many options and search path is also potentially a dupe
+        """If paths_to_search is omitted, defaults to current directory
+        """
+        Cmd.__init__(self)
+        self.paths_to_search = paths_to_search or ['.']
+        self.pt_config = pt_config
+        self.grep_options = grep_options
+
+    def emptyline(self):
+        "NOOP - do not repeat last command like cmd.Cmd"
+        pass
+
+    def do_exit(self, line=None):
+        """Quit/Exit"""
+        print("Quitting...")
+        return 1
+    do_quit = do_exit
+    do_bye = do_exit
+
+    def do_set(self, line=None):
+        """Set variables/options
+
+Examples
+
+    set ic
+    set ignorecase
+    set noic
+    set noignorecase
+
+"""
+        pass
+
+    def do_grep(self, line=None):
+        """ptgrep/search"""
+        pass
+    do_g = do_grep
+
+    def do_config(self, line=None):
+        """show puren tonbo config"""
+        print('%s' % json.dumps(self.pt_config, indent=4, sort_keys=True))  # TODO color support
+
+    def do_version(self, line=None):
+        """show version/info"""
+        puren_tonbo.print_version_info()
+    do_info = do_version
 
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
 
+    usage = "usage: %prog [options] [search_term] [dir_name_or_filename1] [dir_name_or_filename2...]"
+    parser = OptionParser(usage=usage, version="%%prog %s" % puren_tonbo.__version__)
+    parser.add_option("-c", "--codec", help="Override config file encoding (can be a list TODO format comma?)")
+    parser.add_option("--config-file", help="Override config file")
+    parser.add_option("--note-root", help="Directory of notes, or dir_name_or_filename1.... will pick up from config file and default to '.'")
+    parser.add_option("-p", "--password", help="password, if omitted and OS env PT_PASSWORD is set use that, if missing prompt")  # TODO keyring support
+    parser.add_option("-P", "--password_file", help="file name where password is to be read from, trailing blanks are ignored")
+
+    (options, args) = parser.parse_args(argv[1:])
+    if options.password_file:
+        f = open(options.password_file, 'rb')
+        password_file = f.read()
+        f.close()
+        password_file = password_file.strip()
+    else:
+        password_file = None
+
+    password = options.password or password_file or os.environ.get('PT_PASSWORD') or puren_tonbo.caching_console_password_prompt  # TODO keyring support
+    if password and not callable(password) and not isinstance(password, bytes):
+        password = password.encode('us-ascii')
+
+    config = puren_tonbo.get_config(options.config_file)
+    if options.note_root:
+        paths_to_search = [options.note_root]
+    else:
+        paths_to_search = args or [config.get('note_root', '.')]
+        if not paths_to_search:
+            usage_error('ERROR: Missing search path/directory')  # should never happen now, here just-in-case
+
+    if options.codec:
+        note_encoding = options.codec
+    else:
+        note_encoding = config['codec']
+
+
     # TODO refactor ptgrep to allow reuse
-    puren_tonbo.print_version_info()
+    interpreter = CommandPrompt(paths_to_search=paths_to_search, pt_config=config, grep_options=options)  # FIXME ptgrep compat options needed
+    interpreter.onecmd('version')
+    interpreter.cmdloop()
 
     return 0
 
