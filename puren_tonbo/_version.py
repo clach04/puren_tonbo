1a521000b639ad0c81fb79895f03358131e80e70
clach04 <clach04@gmail.com>
2023-04-20 19:42:12 -0700

    pep440 compliant version
    GitHub actions test failed with Python 3
    
    Looks like newer version of setuptools now enforce this at installation time.

--- puren_tonbo/_version.py
+++ puren_tonbo/_version.py
@@ -1,2 +1,2 @@
-version_tuple = __version_info__ = (0, 0, 3, 'git')
+version_tuple = __version_info__ = (0, 0, 'dev3')  # pep-440
 version = version_string = __version__ = '.'.join(map(str, __version_info__))
