c7e4ae294f13824568daafa15f63e9fb77839af5
clach04 <clach04@gmail.com>
2023-07-01 16:35:34 -0700

    Python scite support - #52

---
+++ integrations/pt_scite_with_python/README.md
@@ -0,0 +1,53 @@
+# scite with python
+
+From https://github.com/moltenform/scite-with-python
+
+Tested with 32-bit Windows binaries https://github.com/moltenform/scite-with-python/releases/tag/v0.7.4
+
+Supports loading (decrypting) by spawning  ptcipher.
+
+Does NOT support saving (encrypting).
+
+TODO
+
+  * save support
+  * try importing puren_tonbo instead of using external binary and pipes
+
+## Setup
+
+Important to use the scite properties file shipped with scite-with-python, one option:
+
+
+    (py310venv) C:\code\py\puren_tonbo\scite_with_python_0_7_4_win32>type pyscite.bat
+    @echo off
+    setlocal
+
+    REM from https://github.com/moltenform/scite-with-python
+
+    REM ensure using correct config location
+    REM TODO edit this location to where scite_with_python has been extracted
+    set SciTE_HOME=C:\code\py\puren_tonbo\scite_with_python_0_7_4_win32
+
+    REM modifed (or added files)
+    rem tools_internal\ptplugin\register.properties tools_internal\ptplugin\__init__.py properties\SciTEGlobal.properties
+    REM check  doc\SciTEWithPythonAPIReference.html
+
+    scite %*
+
+    endlocal
+
+Then copy this directory into the scite_with_python root, so end up with:
+
+    C:\....\scite_with_python\pt_scite_with_python
+
+Then edit `properties\SciTEGlobal.properties` and add at the end:
+
+    ## clach04 debug
+    # puren_tonbo plugin from
+    # for use with https://github.com/moltenform/scite-with-python - tested with scite_with_python_0_7_4_win32
+    #import tools_internal/pt_scite_with_python/register
+    import pt_scite_with_python/register
+
+Ensure ptcipher is in the path (or set OS environment variable `PTCIPHER_EXE` to path).
+
+Set password using regular os environment variable, `PT_PASSWORD`, There is no prompt (or keyring) support.
---
+++ integrations/pt_scite_with_python/__init__.py
@@ -0,0 +1,116 @@
+# from https://moltenform.com/page/scite-with-python/doc/writingplugin.html
+# NOTE see register.properties which scite global props needs to include
+
+import os
+import subprocess
+import sys
+
+from scite_extend_ui import ScConst
+from scite_extend_ui import ScEditor  # what is the harm from doing this here?
+
+is_win = sys.platform.startswith('win')
+
+
+PTCIPHER_EXE = os.environ.get('PTCIPHER_EXE', 'ptcipher')
+file_extensions = []
+def determine_encrypted_file_extensions():
+    if is_win:
+        expand_shell = True  # avoid pop-up black CMD window
+    else:
+        expand_shell = False
+
+    cmd = [PTCIPHER_EXE, '--list-formats', '--no-prompt']
+    p = subprocess.Popen(cmd, shell=expand_shell, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout_value, stderr_value = p.communicate()
+
+    print('post com')
+    print('stdout: %r' % stdout_value)
+    print('stderr: %r' % stderr_value)
+    print('returncode: %r' % p.returncode)
+
+    for line in stdout_value.decode('utf-8').replace('\r', '').split('\n')[3:]:
+        line = line.strip()
+        if not line:
+            continue
+        x = line.split(' - ', 1)
+        file_extension = x[0]
+        if file_extension not in ('txt', 'md'):
+            #file_extensions.append(file_extension)
+            file_extensions.append('.' + file_extension)
+    print('stdout: %r' % file_extensions)
+
+def OnBeforeSave(filename):
+    """Save files unless supported encrypted file extension. No support for save/encrypt yet
+    """
+    print('plugin %s file %s' % (__file__, filename))  # goes to output pane
+    for file_extension in file_extensions:
+        if filename.lower().endswith(file_extension):
+            print('REFUSING to save file %s' % (filename,))
+            print('missing save/encrypt support')
+            # TODO encrypt file - send scite buffer via stdio of PTCIPHER_EXE and have PTCIPHER_EXE  write to disk
+            # assuming no failure; ScEditor.SetSavePoint()  # indicate to editor that save happened and file is unchanged
+            return ScConst.StopEventPropagation  # works, tells Scite to NOT save
+    return False  # Scite will handle save
+    #return "StopEventPropagation"  # works
+    #return ScConst.StopEventPropagation  # works, tells Scite to NOT save
+    # https://www.scintilla.org/SciTEExtension.html
+
+def OnSave(filename):
+    print('plugin %s file %s' % (__file__, filename))  # goes to output pane
+    # file has ALREADY been saved at this point
+
+def OnOpen(filename):
+    """Opens a file, if supported file extension attempt to decrypt/load by spawning external tool which expects password to be set in OS variable
+    """
+    print('plugin saw OnOpen', filename)  # goes to output pane
+    is_encrypted = False
+    for file_extension in file_extensions:
+        if filename.lower().endswith(file_extension):
+            is_encrypted = True
+            break
+    if not is_encrypted:
+        return
+
+    if is_win:
+        expand_shell = True  # avoid pop-up black CMD window
+    else:
+        expand_shell = False
+    # TODO prompt for password, for now pick up from env (by default)
+    cmd = [PTCIPHER_EXE, '--silent', '--no-prompt', '--decrypt', filename, '--output=-']
+    # DEBUG code that shows we can inject into the sub-process environment
+    #os.environ['PT_PASSWORD'] = 'bad'
+    #os.environ['PT_PASSWORD'] = 'password'  # works  # FIXME prompt!
+    # re-read from disk using PTCIPHER_EXE, could potential send scite buffer via stdio to avoid the disk IO but easier to re-read.
+    p = subprocess.Popen(cmd, shell=expand_shell, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # time out is py3 (3.3+?)
+    """
+    try:
+        timeout = 15
+        timeout = 3
+        stdout_value, stderr_value = p.communicate(timeout=timeout)
+    except subprocess.TimeoutExpired:
+        p.kill()
+        #stdout_value, stderr_value = p.communicate()  # just hangs again
+        stdout_value, stderr_value = '', ''
+    """
+    stdout_value, stderr_value = p.communicate()
+
+    print('post com')
+    print('stdout: %r' % stdout_value)
+    print('stderr: %r' % stderr_value)
+    print('returncode: %r' % p.returncode)
+    #print('errors: %r' % p.errors)  # py3 only
+
+    # TODO other stuff
+    # for exacmple check exit code and stderr
+
+    #ScEditor.BeginUndoAction()
+    ScEditor.SetText(stdout_value)
+    #ScEditor.EndUndoAction()  # this did not allow undo after all
+    ScEditor.SetSavePoint()  # indicate to editor that save happened and file is unchanged - whether it really did or not ;-)
+
+
+
+print('loaded %s', __file__)  # DEBUG goes to output pane
+determine_encrypted_file_extensions()
+print('completed init %s', __file__)  # DEBUG goes to output pane
