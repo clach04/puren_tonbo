a3950eaad4564165e33199038dd50c9a0b329a21
clach04 <clach04@gmail.com>
2024-01-13 09:54:19 -0800

    Quick and dirty Unicode safe config printer for ptig

--- puren_tonbo/tools/ptig.py
+++ puren_tonbo/tools/ptig.py
@@ -364,6 +364,7 @@ Examples
             for attribute_name in options:
                 if not attribute_name.startswith('_'):
                     attribute_value = options[attribute_name]
+                    attribute_value = repr(attribute_value)  # DEBUG, alternative hack stdout like in ptgrep()
                     #print('\t%s=%s' % (attribute_name, attribute_value))  # TODO consider sorted dict?
                     print('\t%s%s%s=%s%s%s' % (name_color, attribute_name, color_reset, value_color, attribute_value, color_reset))  # TODO consider sorted dict?
             return
