a7f7c512e7b373712ea83c7653967ac854cc6fbe
clach04 <clach04@gmail.com>
2024-09-22 10:50:30 -0700

    Clean up ptconfig parameter usage

--- puren_tonbo/tools/ptconfig.py
+++ puren_tonbo/tools/ptconfig.py
@@ -25,7 +25,7 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv
 
-    usage = "usage: %prog [options] [search_term] [dir_name_or_filename1] [dir_name_or_filename2...]"
+    usage = "usage: %prog [options]"
     parser = OptionParser(usage=usage, version="%%prog %s" % puren_tonbo.__version__)
     parser.add_option("--config-file", "--config_file", help="Override config file")
     parser.add_option("--note-root", help="Override Directory of notes")
@@ -38,13 +38,14 @@ def main(argv=None):
         return 0
 
     config_filename = puren_tonbo.get_config_path()
+    config_filename = options.config_file or puren_tonbo.get_config_path()
     if os.path.exists(config_filename):
         config_filename_exists = True
     else:
         config_filename_exists = False
     print('config_filename %s (%s)' % (config_filename, 'exists' if config_filename_exists else 'does not exist'))
 
-    config = puren_tonbo.get_config(options.config_file)
+    config = puren_tonbo.get_config(config_filename)
     if options.note_root:
             config['note_root'] = options.note_root
     print('%s' % json.dumps(config, indent=4, sort_keys=True))  # TODO color support
