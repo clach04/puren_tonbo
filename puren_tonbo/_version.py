051b760f84a70a7bda686b49b0b03b71bede223f
clach04 <clach04@gmail.com>
2025-01-07 20:47:36 -0800

    version tuple update to match same name as sys.version_info

--- puren_tonbo/_version.py
+++ puren_tonbo/_version.py
@@ -1,2 +1,2 @@
-version_tuple = __version_info__ = (0, 0, 4, 'dev1')  # pep-440
+version_tuple = version_info = __version_info__ = (0, 0, 4, 'dev1')  # pep-440 version numbers, and sys.version_info compatible (unnamed) tuple
 version = version_string = __version__ = '.'.join(map(str, __version_info__))
